37efa18f093704e9843189c6bc40650e155565ea
Morgan Gonzalez <Morgan@Morgans-MacBook-Pro.local>
2016-11-19 12:48:25 -0500

    complete front

--- assets/step.js
+++ assets/step.js
@@ -1,7 +1,7 @@
 
   //*******  Google Map  *******//    
   function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('stepMap'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -33.8688, lng: 151.2195},
       zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -10,7 +10,7 @@
     // Create the search box and link it to the UI element.
     var input = document.getElementById('pac-input');
     var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    
 
     // Bias the SearchBox results towards current map's viewport.
     map.addListener('bounds_changed', function() {
