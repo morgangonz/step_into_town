9936ae9acfbcfec61fb6af2b1ffc043d6b472bd8
NAO <Nao@NaoKosaka.local>
2016-11-19 19:56:57 -0500

    Added Geo Location API, current location button(click function)

--- assets/step.js
+++ assets/step.js
@@ -1,71 +1,136 @@
 
-  //*******  Google Map  *******//    
-  function initAutocomplete() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -33.8688, lng: 151.2195},
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+//*********  Geo location  **********//
+$("#currentLocation").on("click",function(){
 
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    
+  // check for Geolocation support
+  if(navigator.geolocation){
+    //get current position
+    navigator.geolocation.getCurrentPosition(
 
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
+      //when success 
+      function(position){
+        // data
+        var data = position.coords;
+        var lat = data.latitude;
+        var lng = data.longitude;
+        var alt = data.altitude;
+        var accLatlng = data.accuracy;
+        var accAlt = data.altitudeAccuracy;
+        //0=N,90=E,180=S,270=W
+        var heading = data.heading;
+        var speed = data.speed;
 
-    var markers = [];
-    // [START region_getplaces]
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-      var places = searchBox.getPlaces();
+        // bug check
+        //console.log(lat+lng) ;
 
-      if (places.length == 0) {
-        return;
-      }
+        var latlng = new google.maps.LatLng(lat,lng);
 
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
+        // update latlng location on google maps
+        var map = new google.maps.Map(document.getElementById('map'),{
+          zoom: 15,      
+          center: latlng,
+        });
 
-      // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        // Create a marker for each place.
-        markers.push(new google.maps.Marker({
+        // put a pin
+        new google.maps.Marker({
           map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
+          position: latlng,
+        } ) ;
+      },
 
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-    // [END region_getplaces]
+      //when error
+      function(error)
+      {
+        //error comment
+        var errorInfo =[
+          "UNKNOWN ERROR",
+          "PERMISSION DENIED",
+          "POSITION UNAVAILABLE",
+          "TIMEOUT"
+        ];
 
+        //error number
+        var errorNo = error.code;
+
+        //error comment
+        var errorMessage = "Sorry!!! " + errorInfo[errorNo];
+
+        // bug check
+        //console.log(errorMessage);
+        document.getElementById("result").innerHTML = errorMessage ;
+      }
+    );
   };
-  //*******  Finish Google Map  *******//
+});
+
+
+
+//*************  Google Map  *************//    
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 24.116675, lng: -19.160156},
+    zoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+};
+//**********  Finish Google Map  *************//
 
 
 //***************** Code for Sqoot API *******************
