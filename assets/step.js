488c66741e92dbd14d39ce51e0b03e8fdbda45fb
NAO <Nao@NaoKosaka.local>
2016-11-29 21:23:15 -0500

    pull

--- assets/step.js
+++ assets/step.js
@@ -29,75 +29,7 @@ $('#deals').on('click',function(){
       var autocomplete;
       var countryRestrict = {'country': 'us'};
       var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
-      var hostnameRegexp = new RegExp('^https?://.+?/');
-
-      //When the user selects a country, set center and zoom the country on the map.
-      var countries = {
-        'au': {
-          center: {lat: -25.3, lng: 133.8},
-          zoom: 4
-        },
-        'br': {
-          center: {lat: -14.2, lng: -51.9},
-          zoom: 3
-        },
-        'ca': {
-          center: {lat: 62, lng: -80.0},
-          zoom: 3
-        },
-        'cn': {
-          center: {lat: 35.8, lng: 104.1},
-          zoom: 4
-        },
-        'fr': {
-          center: {lat: 46.2, lng: 2.2},
-          zoom: 5
-        },
-        'de': {
-          center: {lat: 51.2, lng: 10.4},
-          zoom: 5
-        },
-        'jp': {
-          center: {lat: 37.5, lng: 138.2},
-          zoom: 5
-        },
-        'my': {
-          center: {lat: 4.2, lng: 101.9},
-          zoom: 5
-        },
-        'mx': {
-          center: {lat: 23.6, lng: -102.5},
-          zoom: 4
-        },
-        'nz': {
-          center: {lat: -40.9, lng: 174.9},
-          zoom: 5
-        },
-        'it': {
-          center: {lat: 41.9, lng: 12.6},
-          zoom: 5
-        },
-        'za': {
-          center: {lat: -30.6, lng: 22.9},
-          zoom: 5
-        },
-        'es': {
-          center: {lat: 40.5, lng: -3.7},
-          zoom: 5
-        },
-        'pt': {
-          center: {lat: 39.4, lng: -8.2},
-          zoom: 6
-        },
-        'us': {
-          center: {lat: 37.1, lng: -95.7},
-          zoom: 3
-        },
-        'uk': {
-          center: {lat: 54.8, lng: -4.6},
-          zoom: 5
-        },
-      };
+      
 
       //map style
       function initMap() {
@@ -129,9 +61,7 @@ $('#deals').on('click',function(){
 
         autocomplete.addListener('place_changed', onPlaceChanged);
 
-        // Add a DOM event listener to react when the user selects a country.
-        document.getElementById('country').addEventListener(
-            'change', setAutocompleteCountry);
+        
       }
 
       // When the user selects a city, get the place details for the city and
@@ -209,23 +139,6 @@ $('#deals').on('click',function(){
           }
         }
         markers = [];
-      }
-
-      // Set the country restriction based on user input.
-      // Also center and zoom the map on the given country.
-      function setAutocompleteCountry() {
-        var country = document.getElementById('country').value;
-        if (country == 'all') {
-          autocomplete.setComponentRestrictions([]);
-          map.setCenter({lat: 20.463426, lng: -3.558594});
-          map.setZoom(2);
-        } else {
-          autocomplete.setComponentRestrictions({'country': country});
-          map.setCenter(countries[country].center);
-          map.setZoom(countries[country].zoom);
-        }
-        clearResults();
-        clearMarkers();
       }
 
       function dropMarker(i) {
