65c96e88bce76a8f44ed51e43be2410c9925e3c4
NAO <Nao@NaoKosaka.local>
2016-12-03 00:13:41 -0500

    Delete rate in the information

--- assets/step.js
+++ assets/step.js
@@ -223,24 +223,6 @@ function buildIWContent(place) {
     document.getElementById('iw-phone-row').style.display = 'none';
   }
 
-  // Assign a five-star rating to restaurant, using a black star ('&#10029;')
-  // to indicate the rating the restaurant has earned, and a white star ('&#10025;')
-  // for the rating points not achieved.
-  if (place.rating) {
-    var ratingHtml = '';
-    for (var i = 0; i < 5; i++) {
-      if (place.rating < (i + 0.5)) {
-        ratingHtml += '&#10025;';
-      } else {
-        ratingHtml += '&#10029;';
-      }
-    document.getElementById('iw-rating-row').style.display = '';
-    document.getElementById('iw-rating').innerHTML = ratingHtml;
-    }
-  } else {
-    document.getElementById('iw-rating-row').style.display = 'none';
-  }
-
   // The regexp isolates the first part of the URL (domain plus subdomain)
   // to give a short URL for displaying in the info window.
   if (place.website) {
